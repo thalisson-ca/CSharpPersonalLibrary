e46a0fafb78ae3630d42f3b970acc23048f48f85
Thalisson <thalissonca@gmail.com>
2018-06-19 11:34:36 -0300

    Finished EasyEpochTimeStamp

--- CSharpPersonalLibrary/CSharpPersonalLibrary/Basic/Date/EasyEpochTimeStamp.cs
+++ CSharpPersonalLibrary/CSharpPersonalLibrary/Basic/Date/EasyEpochTimeStamp.cs
@@ -9,24 +9,27 @@ namespace CSharpPersonalLibrary.Basic.Date
     public class EasyEpochTimeStamp
     {
         /// <summary>
-        /// Converts a Epoch Time value to DateTime
+        /// Converts a Epoch Time value to DateTime. 
         /// </summary>
-        /// <param name="epochSeconds">Epoch Time value in seconds</param>
+        /// <param name="epochSeconds">Epoch Time value in seconds. Range between -62135596800 and 253402300799.</param>
         /// <returns>A UTC DateTime for the correspondent epochSeconds</returns>
+        /// <exception cref="ArgumentOutOfRangeException">The epochSeconds param is out of range between -62135596800 and 253402300799</exception>
         public static DateTime ConvertEpochToDateTime(long epochSeconds)
         {
-            return GetEpochTimeZero().AddSeconds(epochSeconds);
-
+            return GetEpochTimeZero().AddSeconds(epochSeconds); 
         }
 
         /// <summary>
-        /// Convert a DateTime to the Epoch Time value
+        /// Convert a DateTime to the Epoch Time valueT
         /// </summary>
         /// <param name="dateTime">A DateTime value to be converted</param>
-        /// <returns>A long value with the Epoch Time value</returns>
+        /// <returns>A long value with the Epoch Time value in seconds</returns>
         public static long ConvertDateTimeToEpoch(DateTime dateTime)
         {
+              
             TimeSpan dif = dateTime.ToUniversalTime() - GetEpochTimeZero();
+            dif = dif.Subtract(new TimeSpan(dif.Ticks%10000000));  //This is because if it has at least 1 tick more, it will round up the seconds.
+            
             return (long)dif.TotalSeconds;
         }
 
--- CSharpPersonalLibrary/Testes/Basic/Date/EasyEpochTimeStampTest.cs
+++ CSharpPersonalLibrary/Testes/Basic/Date/EasyEpochTimeStampTest.cs
@@ -27,6 +27,15 @@ namespace Testes.Basic.Date
             value = EasyEpochTimeStamp.ConvertDateTimeToEpoch(DateTime.MinValue);
             expected = -62135589600;
             Assert.AreEqual(expected, value);
+
+            value = EasyEpochTimeStamp.ConvertDateTimeToEpoch(DateTime.MaxValue);
+            expected = 253402300799;
+            Assert.AreEqual(expected, value);
+
+            value = EasyEpochTimeStamp.ConvertDateTimeToEpoch(new DateTime(1970,1,1,0,0,0,DateTimeKind.Utc));
+            expected = 0;
+            Assert.AreEqual(expected, value);
+
         }
 
         [TestMethod]
@@ -37,12 +46,15 @@ namespace Testes.Basic.Date
             Assert.AreEqual(EasyEpochTimeStamp.GetEpochTimeZero(), value);
 
             value = EasyEpochTimeStamp.ConvertEpochToDateTime(253402300799);
-            DateTime expected = DateTime.MaxValue;
+            DateTime expected = DateTime.MaxValue.AddMilliseconds(-999).AddTicks(-9999);
             Assert.AreEqual(expected, value);
 
-            value = EasyEpochTimeStamp.ConvertEpochToDateTime(-62135589600);
+            value = EasyEpochTimeStamp.ConvertEpochToDateTime(-62135596800);
             expected = DateTime.MinValue;
             Assert.AreEqual(expected, value);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(()=> EasyEpochTimeStamp.ConvertEpochToDateTime(253402300800));
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => EasyEpochTimeStamp.ConvertEpochToDateTime(-62135596801));
 
         }
 
